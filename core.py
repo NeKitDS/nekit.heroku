2627428b6c9e9050f36680e116e284bdb5e763a1
Nikita Tikhonov <nekitguypro@gmail.com>
2020-07-02 18:37:26 +0300

    Initial and first commit.

---
+++ core.py
@@ -0,0 +1,24 @@
+import os
+
+import gd
+import gdbot
+import nekit_site
+
+# get credentials
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GD_USER = os.getenv("GD_USER")
+GD_PASSWORD = os.getenv("GD_PASSWORD")
+
+# create a loop
+LOOP = gd.utils.acquire_loop()
+
+# attach bot running task
+LOOP.create_task(gdbot.run_bot(BOT_TOKEN, GD_USER, GD_PASSWORD, LOOP))
+# attach site running task
+LOOP.create_task(nekit_site.run_async())
+
+try:
+    LOOP.run_forever()
+
+finally:
+    gd.utils.shutdown_loop(LOOP)
