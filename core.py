94c3784d5d3cbd96cc8d07213e604ff3df6eaa77
Nikita Tikhonov <NeKitDS@users.noreply.github.com>
2020-08-08 20:37:04 +0300

    Add factory sheets reloading.

--- core.py
+++ core.py
@@ -12,8 +12,18 @@ GD_PASSWORD = os.getenv("GD_PASSWORD")
 # create a loop
 LOOP = gd.utils.acquire_loop()
 
+
+@gd.tasks.loop(hours=1, loop=LOOP)
+async def reload_images() -> None:
+    for sheet in (gd.factory.icon_sheet, gd.factory.glow_sheet):
+        sheet.image = gd.icon_factory.Image.open(sheet.sheet_path)
+
+
+reload_images.start()
+
 # attach bot running task
 LOOP.create_task(gdbot.run_bot(BOT_TOKEN, GD_USER, GD_PASSWORD, LOOP))
+
 # attach site running task
 LOOP.create_task(nekit_site.run_async())
 
