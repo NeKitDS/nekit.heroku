ecffcb00accfaa11f7302f26267ee4b1e21ac540
Nikita Tikhonov <nekitguypro@gmail.com>
2020-08-17 18:54:41 +0300

    Attempt to remove factory loading.

--- core.py
+++ core.py
@@ -9,9 +9,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 GD_USER = os.getenv("GD_USER")
 GD_PASSWORD = os.getenv("GD_PASSWORD")
 
-# why not?
-gd.factory.load()
-
 # create a loop
 LOOP = gd.utils.acquire_loop()
 
